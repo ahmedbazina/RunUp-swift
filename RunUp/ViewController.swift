40ca40e17fff5c059a8eda718cbec102c568770b
Ahmed Bazina <ahmedbazina98@yahoo.com>
2019-11-13 18:53:09 -0800

    Initial Commit

---
+++ RunUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunUp
+//
+//  Created by Ahmed Bazina on 13/11/2019.
+//  Copyright © 2019 Ahmed Bazina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
